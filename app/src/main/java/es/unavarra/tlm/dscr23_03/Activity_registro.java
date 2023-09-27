8592fc5bb82137c00d413cf9959b02096666f554
Yanfang <sun.141867@e.unavarra.es>
2023-09-27 20:21:26 +0200

    agrego algunos codigos sobre password, linea 26 y 30 elimino los codigos sobre la función verValor en la parte inferior.

--- app/src/main/java/es/unavarra/tlm/dscr23_03/Activity_registro.java
+++ app/src/main/java/es/unavarra/tlm/dscr23_03/Activity_registro.java
@@ -2,19 +2,13 @@ package es.unavarra.tlm.dscr23_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import es.unavarra.tlm.dscr23_03.R.id;
 
 public class Activity_registro extends AppCompatActivity {
-
-    public String nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,35 +16,18 @@ public class Activity_registro extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
 
 
+        EditText usuario = findViewById(R.id.user_name);
+        EditText password = findViewById(R.id.password);
 
 
         Button button = (Button) findViewById(R.id.boton_crear);
-        button.setOnClickListener(new OpenActivityCrear(this));
+        button.setOnClickListener(new OpenActivityCrear(this, usuario, password));
 
         //EditText usuario = findViewById(id.user_name);
         //usuario.setOnTouchListener(new TouchListener(this));
 
         Log.d("myActivity", "setNombre: 123");
 
-
-
-
     }
 
-    public void setNombre (){
-        String username= verValor();
-        this.nombre = username;
-        Log.d("myActivity", "setNombre: "+nombre);
-    }
-
-    public String getNombre(){
-        return nombre;
-    }
-    public String verValor (){
-        EditText usuario =  findViewById(R.id.user_name);
-        String nombre= usuario.getText().toString();
-        Log.d("nombre", nombre);
-
-        return nombre;
-    }
 }
